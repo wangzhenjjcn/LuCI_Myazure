cbd9cb3eef9884ef470fbf5b7affa4e61977c00c
WangZhen <wangzhenjjcn@gmail.com>
2021-11-24 21:49:16 +0800

    0

--- usr/lib/lua/luci/version.lua
+++ usr/lib/lua/luci/version.lua
@@ -16,5 +16,5 @@ else
 	distversion = "Development Snapshot"
 end
 
-luciname    = "WangZhenLuCIa"
-luciversion = "j8u2r48uhBETA"
+luciname    = "LuCI Master"
+luciversion = "git-20.117.60969-420c61a"
