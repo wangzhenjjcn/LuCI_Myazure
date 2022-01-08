1dac72cacd67dcd3626d8c66edbc5940fede7422
WangZhen <wangzhenjjcn@gmail.com>
2022-01-09 06:40:12 +0800

    init xiaomi4ag

--- usr/lib/lua/luci/controller/webadmin.lua
+++
@@ -1,9 +0,0 @@
-module("luci.controller.webadmin",package.seeall)
-function index()
-if not nixio.fs.access("/etc/config/uhttpd")then
-return
-end
-local e
-e=entry({"admin","system","webadmin"},cbi("webadmin"),_("Web Admin"),1)
-e.leaf=true
-end
