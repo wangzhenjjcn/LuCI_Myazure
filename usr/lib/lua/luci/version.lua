53f5a02c2e452e424e8a7f379621e62b3492906a
WangZhen <wangzhenjjcn@gmail.com>
2022-01-14 00:47:23 +0800

    1

---
+++ usr/lib/lua/luci/version.lua
@@ -0,0 +1,11 @@
+local pcall, dofile, _G = pcall, dofile, _G
+
+module "luci.version"
+
+
+distname    = "Myazure"
+distversion = "MYAZURECloud"
+
+
+luciname    = "Hongkong"
+luciversion = "20220114"
