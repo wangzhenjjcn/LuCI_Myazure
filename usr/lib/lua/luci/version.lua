653265bace077d8685b2013b8486c4dced7a7581
WangZhen <wangzhenjjcn@gmail.com>
2022-01-13 21:06:03 +0800

    1

---
+++ usr/lib/lua/luci/version.lua
@@ -0,0 +1,12 @@
+local pcall, dofile, _G = pcall, dofile, _G
+
+module "luci.version"
+
+
+
+distname    = "Myazure"
+distversion = "HongKong"
+
+
+luciname    = "MyazureUI"
+luciversion = "20220113"
