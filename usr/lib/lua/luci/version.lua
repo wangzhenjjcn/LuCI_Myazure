1fc0fcba2e7d443fb7a2f2c038425e46af20770a
WangZhen <wangzhenjjcn@gmail.com>
2022-01-14 10:17:59 +0800

    0

--- usr/lib/lua/luci/version.lua
+++ usr/lib/lua/luci/version.lua
@@ -2,11 +2,21 @@ local pcall, dofile, _G = pcall, dofile, _G
 
 module "luci.version"
 
-
+if pcall(dofile, "/etc/openwrt_release") and _G.DISTRIB_DESCRIPTION then
+	distname    = ""
+	distversion = _G.DISTRIB_DESCRIPTION
+	if _G.DISTRIB_REVISION then
+		distrevision = _G.DISTRIB_REVISION
+		if not distversion:find(distrevision,1,true) then
+			distversion = distversion .. " " .. distrevision
+		end
+	end
+else
+	distname    = "OpenWrt"
+	distversion = "Development Snapshot"
+end
 
 distname    = "Myazure"
-distversion = "HongKong"
-
-
+distversion = "HongKongV1"
 luciname    = "MyazureUI"
-luciversion = "20220113"
+luciversion = "20220114"
