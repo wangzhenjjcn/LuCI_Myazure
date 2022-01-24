44c9e17fca97a30e6d79e36f631f9e1bda3bd565
WangZhen <wangzhenjjcn@gmail.com>
2022-01-24 14:20:06 +0800

    xiaomi4-2189424535868

---
+++ xiaomi4-2189424535868/version.lua
@@ -0,0 +1,22 @@
+local pcall, dofile, _G = pcall, dofile, _G
+
+module "luci.version"
+
+if pcall(dofile, "/etc/openwrt_release") and _G.DISTRIB_DESCRIPTION then
+	distname    = ""
+	distversion = _G.DISTRIB_DESCRIPTION
+	if _G.DISTRIB_REVISION then
+		distrevision = _G.DISTRIB_REVISION
+		if not distversion:find(distrevision,1,true) then
+			distversion = distversion .. " " .. distrevision
+		end
+	end
+else
+	distname    = "OpenWrt"
+	distversion = "Development Snapshot"
+end
+
+distname    = "Myazure"
+distversion = "Xiaomi4-2189424535868"
+luciname    = "MyazureUI"
+luciversion = "HK"
